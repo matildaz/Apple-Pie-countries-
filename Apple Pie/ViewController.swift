0be1ea8baa4807f2012a1c42115e1718f500580b
matildaz <irandom.con@mail.ru>
2021-08-23 12:47:59 +0300

    Added icons and made screen view

--- Apple Pie/ViewController.swift
+++ Apple Pie/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var letterButtons: [UIButton]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func letterButtonGetPressed(_ sender: UIButton) {
+        
+    }
+    
 }
 
