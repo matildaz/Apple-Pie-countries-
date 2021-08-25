fc912bcab5b4bfaf1963e49e686a404e6321b7f5
matildaz <irandom.con@mail.ru>
2021-08-25 12:28:48 +0300

    words can be guessed

--- Apple Pie/StructsOfApple-Pie.swift
+++ Apple Pie/StructsOfApple-Pie.swift
@@ -8,4 +8,30 @@
 struct Game {
     var word : String
     var incorrectMovesRemaining : Int
+    fileprivate var listOfCharacters = Array<Character>()
+    
+    init(word: String, incorrectMovesRemaining: Int) {
+        self.word = word
+        self.incorrectMovesRemaining = incorrectMovesRemaining
+    }
+    
+    var guessedWord: String {
+        var wordToShow: String = ""
+        for letter in word {
+            if listOfCharacters.contains(Character(letter.lowercased())) {
+                wordToShow += String(letter)
+            } else {
+                wordToShow += "_"
+            }
+        }
+        return wordToShow
+    }
+    
+    mutating func letterGuees(letter:Character) {
+        let lowercasedLetter = letter.lowercased()
+        listOfCharacters += lowercasedLetter
+        if !word.lowercased().contains(lowercasedLetter) {
+            incorrectMovesRemaining -= 1
+        }
+    }
 }
